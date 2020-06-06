755074cb808e011da7a5e35ef6c377c88b115cc2
greymd <yamadagrep@gmail.com>
2020-06-06 10:57:48 +0100

    Use tar instead of bzip2 crate

--- libpcre-sys/build.rs
+++ libpcre-sys/build.rs
@@ -6,17 +6,13 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-extern crate bzip2;
 extern crate pkg_config;
 extern crate tar;
 
-use bzip2::read::{BzDecoder};
 use std::env;
-use std::fs::{OpenOptions};
 use std::io::{ErrorKind};
 use std::path::{Path};
 use std::process::{Command};
-use tar::{Archive};
 
 const BUNDLED_PCRE_VERSION: &'static str = "8.39";
 
@@ -31,13 +27,20 @@ fn main() {
     let ext_pathbuf = Path::new(&cargo_manifest_dir).join("ext");
 
     let pcre_tbz2_pathbuf = ext_pathbuf.join(format!("pcre-{}.tar.bz2", BUNDLED_PCRE_VERSION));
-    let pcre_tbz2_f = OpenOptions::new().read(true).open(pcre_tbz2_pathbuf).unwrap();
-    let decompressor = BzDecoder::new(pcre_tbz2_f);
-
-    let mut archive = Archive::new(decompressor);
-    if !archive.unpack(&out_dir).is_ok() {
-        panic!("failed to extract the tarball");
-    }
+    let mut tar_cmd = Command::new("tar");
+    tar_cmd.arg("jxvf");
+    tar_cmd.arg(format!("{}", &pcre_tbz2_pathbuf.as_os_str().to_string_lossy()));
+    tar_cmd.arg("-C");
+    tar_cmd.arg(&out_dir);
+    match tar_cmd.status() {
+            Err(ref e) if e.kind() == ErrorKind::NotFound => {
+                panic!("failed to execute `tar`: {}. Is `tar` installed?", e);
+            },
+            Err(e) => {
+                panic!("failed to execute `tar`: {}", e);
+            },
+            Ok(status) => status
+    };
 
     let pcre_pathbuf = Path::new(&out_dir).join(format!("pcre-{}", BUNDLED_PCRE_VERSION));
 
@@ -70,7 +73,7 @@ fn main() {
             Err(e) => {
                 panic!("failed to execute `./configure`: {}", e);
             },
-            Ok(status) => status
+            Ok(status) => status,
         };
         if !status.success() {
             panic!("`./configure --with-pic ...` did not run successfully.");
